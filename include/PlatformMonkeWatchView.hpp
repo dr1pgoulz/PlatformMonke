de6ab052eb43bb801bcacbce972773d054424b27
Waulta <81683867+Waulta@users.noreply.github.com>
2021-07-04 18:09:06 +0930

    Add files via upload

--- include/PlatformMonkeWatchView.hpp
+++ include/PlatformMonkeWatchView.hpp
@@ -3,6 +3,7 @@
 #include "custom-types/shared/macros.hpp"
 #include "monkecomputer/shared/ViewLib/View.hpp"
 #include "monkecomputer/shared/InputHandlers/UIToggleInputHandler.hpp"
+#include "monkecomputer/shared/InputHandlers/UISelectionHandler.hpp"
 
 DECLARE_CLASS_CODEGEN(PlatformMonke, PlatformMonkeWatchView, GorillaUI::Components::View, 
     DECLARE_METHOD(void, Awake);
@@ -11,9 +12,14 @@ DECLARE_CLASS_CODEGEN(PlatformMonke, PlatformMonkeWatchView, GorillaUI::Componen
     DECLARE_METHOD(void, DrawHeader);
     DECLARE_METHOD(void, DrawBody);
     DECLARE_METHOD(void, OnKeyPressed, int key);
-    DECLARE_METHOD(void, OnToggle, bool value);
+    DECLARE_METHOD(void, OnEnter, int key);
     public:
+        GorillaUI::UISelectionHandler* settingSelector = nullptr;
         GorillaUI::UIToggleInputHandler* toggleHandler = nullptr;
+        GorillaUI::UISelectionHandler* leftPlatSizeSelector = nullptr;
+        GorillaUI::UISelectionHandler* rightPlatSizeSelector = nullptr;
+        GorillaUI::UISelectionHandler* leftColorSelector = nullptr;
+        GorillaUI::UISelectionHandler* rightColorSelector = nullptr;
     REGISTER_FUNCTION(
         REGISTER_METHOD(Awake);
         REGISTER_METHOD(DidActivate);
@@ -21,6 +27,6 @@ DECLARE_CLASS_CODEGEN(PlatformMonke, PlatformMonkeWatchView, GorillaUI::Componen
         REGISTER_METHOD(DrawHeader);
         REGISTER_METHOD(DrawBody);
         REGISTER_METHOD(OnKeyPressed);
-        REGISTER_METHOD(OnToggle);
+        REGISTER_METHOD(OnEnter);
     )
 )
--- include/config.hpp
+++ include/config.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
 struct config_t {
+    int leftPlatSize = 0;
+    int rightPlatSize = 0;
+    int leftColor = 0;
+    int rightColor = 0;
     bool enabled = false;
 };
 
